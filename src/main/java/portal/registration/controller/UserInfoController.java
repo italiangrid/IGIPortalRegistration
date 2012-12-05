8ddff9a9fcee5a137ee9e0ff17698cf67b03e887
dmichelotto <diego.michelotto@cnaf.infn.it>
2012-12-05 13:36:23 +0100

    removed warning

--- src/main/java/portal/registration/controller/UserInfoController.java
+++ src/main/java/portal/registration/controller/UserInfoController.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 import portal.registration.utils.GuseNotify;
